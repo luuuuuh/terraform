4c8652e22dec628bbfbc1a2c9c589efffe8aec7a
Luiz <look_lf@hotmail.com>
2019-12-12 12:16:10 -0300

    atualizado

--- main.tf
+++ main.tf
@@ -1,12 +1,16 @@
 provider "aws" {
   region = "us-west-2"
+  #Credenciais, substituir
   shared_credentials_file = "/Users/Luiz/.aws/credentials"
   profile = "default"
 }
 resource "aws_instance" "UniZabbix" {
-  ami = "ami-0ffb8a7104d2b2f57"
+  #Ami aws para Docker
+  ami = "ami-08d489468314a58df"
   instance_type = "t2.micro"
+  #Grupo ja criado liberando determnadas portas
   vpc_security_group_ids = ["sg-08ce3278967b196b5"]
+  #nome da Chave criada
   key_name = "oregon2"
   root_block_device {
            delete_on_termination = true 
